e51a84366331bc38ccb6734946258344b1f44aa7
药音 <ziwen.fzw@alibaba-inc.com>
2016-07-11 21:17:34 +0800

    real time list

--- app.js
+++ app.js
@@ -24,40 +24,39 @@ app.use(serve(path.join(__dirname, 'public')));
 // Compress
 app.use(compress());
 
-
-
-
-
-
-
 /********* socket io *********/
 // attach socket io after all the app.use
 // const server = require('http').Server(app.callback());
 // const io = require('socket.io')(server);
 
 var io = require('socket.io').listen(app.listen(3002)); //wocao...!!!
-var activeRooms = [];
+var activeMusixiserId = [];//as index... temporarily
+var activeMusixiserInfo = [];
 
 io.on('connection', function(socket) {
     var addedUser = false;
-    // musician create a stage
-    //best have a userauth here
+    // musician create a stage, best have a userauth here
     //艹，应该stage名跟着nickname好，还是谁抢占快。应该第一种吧,现在是按照第二种逻辑，但第一种也许更合理？！？！？！？！？！
-    socket.on('create stage', function(nickname) {
-        // if (activeRooms.indexOf(nickname)==-1) {
+    socket.on('create stage', function(userInfo) {
+        var nickname = userInfo.name;
+        // if (activeMusixiserId.indexOf(nickname)==-1) {
         socket.join(nickname);
-        activeRooms.push(nickname);
-        console.log('stage ' + nickname + ' created');
+        activeMusixiserId.push(nickname);
+        activeMusixiserInfo.push(userInfo);
+        console.log('stage ' + nickname + ' created'+JSON.stringify(userInfo));
+        io.emit('moreActiveMusician',userInfo); //用this或socket，stagelist端收不到。use io will send to every where, 也许不是design pattern....凑活先用
         // } else {
         // socket.emit('dup stage');
         // }
         this.on('disconnect', function() {
             console.log('musixiser' + nickname + 'disconnect');
+            io.emit('lessActiveMusician',nickname);
             this.broadcast.to(nickname).emit('no stage');
-            var l = activeRooms.length;
+            var l = activeMusixiserId.length;
             for (var i = 0; i <= l - 1; i++) {
-                if (activeRooms[i] == nickname) {
-                    activeRooms.splice(i, 1);
+                if (activeMusixiserId[i] == nickname) {
+                    activeMusixiserId.splice(i, 1);
+                    activeMusixiserInfo.splice(i, 1);
                     break;
                 }
             }
@@ -85,8 +84,15 @@ io.on('connection', function(socket) {
 
     // audience enter a stage
     socket.on('audienceEnterStage', function(nickname) {
+        for (var i = 0;i<=activeMusixiserId.length-1;i++) {
+            if (activeMusixiserId[i]==nickname) {
+                activeMusixiserInfo[i].audienceNum += 1;
+                break;
+            }
+        }
         //make sure audience enter a created stage
-        if (activeRooms.indexOf(nickname) >= 0) {
+        io.emit('audienceNumUpdate',{nickname:nickname,amountdiff:1});
+        if (activeMusixiserId.indexOf(nickname) >= 0) {
             this.join(nickname);
             this.broadcast.to(nickname).emit('AudienceCome');
             console.log('stage ' + nickname + ' entered');
@@ -103,13 +109,20 @@ io.on('connection', function(socket) {
         });
         this.on('disconnect', function() { //not real disconnect
             // console.log('audience leave a stage');
-            // this.broadcast.to(nickname).emit('AudienceLeave');
+            this.broadcast.to(nickname).emit('AudienceLeave');
+            io.emit('audienceNumUpdate',{nickname:nickname,amountdiff:-1});
         });
         this.on('req_AudienceLeaveRoom', function() {
+            //也要做对应的人数更新操作呀呀呀
             this.broadcast.to(nickname).emit('res_AudienceLeaveRoom');
             this.leave(nickname);
         })
 
+    });
+
+    // audience enter app, checking online musician list
+    socket.on('audienceEnterApp',function(){
+        this.emit('currentMusicianList',activeMusixiserInfo);
     });
 
 });
